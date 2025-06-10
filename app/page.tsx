a21eb7237d2ab215fd3ab70077cd12e20300ca3c
xngar <mantonio.zr@gmail.com>
2025-06-10 16:41:02 -0400

    agregada animaciones

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,103 @@
+"use client";
+import { motion } from "framer-motion";
 import Empresas from "@/componentes/Empresas";
 import Footer from "@/componentes/Footer";
 import Header from "@/componentes/Header";
+
+const variants = {
+  hidden: { opacity: 0, y: 100 },
+  visible: { opacity: 1, y: 0 },
+};
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <Empresas
-        imagen="/jovenes_2.jpg"
-        nombre=""
-        descripcion="Programas turisticos Internacionales y Giras de Estudio"
-        logo="/gira.png"
-        color="#E76218"
-        reverse={false}
-        vinculo="https://migira.cl/"
-      />
-      <Empresas
-        imagen="/bg-once.jpg"
-        nombre=""
-        descripcion="Construcción de proyectos públicos y privados | Proyectos inmobiliarios"
-        logo="/logo-once-61.png"
-        color="#E76218"
-        reverse={true}
-        vinculo="https://once61.cl/"
-      />
-      <Empresas
-        imagen="/bg-rental.jpg"
-        nombre=""
-        descripcion="Arriendos de Camionetas | Arriendo de Vans | Arriendo de Camiones Pluma | Arriendo de Camiones Aljibe | Arriendo de Camiones Tolva"
-        logo="/logo_rental.png"
-        color="#E76218"
-        reverse={false}
-        vinculo="https://rentalspa.cl/"
-      />
-      <Empresas
-        imagen="/bg-energy.jpg"
-        nombre=""
-        descripcion="Venta de aguas y bebidas nutricionales"
-        logo="/logo_energy.png"
-        color="#E76218"
-        reverse={true}
-        vinculo=""
-      />
-      <Empresas
-        imagen="/bg-gou.jpg"
-        nombre=""
-        descripcion="Venta de productos de construcción | Venta de neumáticos y accesorios para vehículos "
-        logo="/logo_gouap.png"
-        color="#E76218"
-        reverse={false}
-        vinculo=""
-      />
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{ duration: 1 }}
+        variants={variants}
+      >
+        <Empresas
+          imagen="/jovenes_2.jpg"
+          nombre=""
+          descripcion="Programas turisticos Internacionales y Giras de Estudio"
+          logo="/gira.png"
+          color="#E76218"
+          reverse={false}
+          vinculo="https://migira.cl/"
+        />
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{ duration: 1 }}
+        variants={variants}
+      >
+        <Empresas
+          imagen="/bg-once.jpg"
+          nombre=""
+          descripcion="Construcción de proyectos públicos y privados | Proyectos inmobiliarios"
+          logo="/logo-once-61.png"
+          color="#E76218"
+          reverse={true}
+          vinculo="https://once61.cl/"
+        />
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{ duration: 1 }}
+        variants={variants}
+      >
+        <Empresas
+          imagen="/bg-rental.jpg"
+          nombre=""
+          descripcion="Arriendos de Camionetas | Arriendo de Vans | Arriendo de Camiones Pluma | Arriendo de Camiones Aljibe | Arriendo de Camiones Tolva"
+          logo="/logo_rental.png"
+          color="#E76218"
+          reverse={false}
+          vinculo="https://rentalspa.cl/"
+        />
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{ duration: 1 }}
+        variants={variants}
+      >
+        <Empresas
+          imagen="/bg-energy.jpg"
+          nombre=""
+          descripcion="Venta de aguas y bebidas nutricionales"
+          logo="/logo_energy.png"
+          color="#E76218"
+          reverse={true}
+          vinculo=""
+        />
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{ duration: 1 }}
+        variants={variants}
+      >
+        <Empresas
+          imagen="/bg-gou.jpg"
+          nombre=""
+          descripcion="Venta de productos de construcción | Venta de neumáticos y accesorios para vehículos "
+          logo="/logo_gouap.png"
+          color="#E76218"
+          reverse={false}
+          vinculo=""
+        />
+      </motion.div>
       <Footer />
     </div>
   );
